d73ed91b6b8d3438179ba4d4e083292f6cd38f56
Rinat0880 <rinatmambetlepesov@gmail.com>
2025-03-29 20:05:06 +0500

    a slight change

--- main.cpp
+++ main.cpp
@@ -1,74 +1,125 @@
 #include <Windows.h>
 #include <iostream>
+#include <thread>
 
 int ScreenX = GetSystemMetrics(SM_CXSCREEN);
 int ScreenY = GetSystemMetrics(SM_CYSCREEN);
-
 int CrossHairColor = RGB(255, 0, 0);
+bool Running = true;
 
 void DrawCrosshair(HDC hdc, int centerX, int centerY, int length, int thickness)
 {
-	HPEN hpen = CreatePen(PS_SOLID, thickness, CrossHairColor);
-	HPEN holdpen = (HPEN)SelectObject(hdc, hpen);
+    HPEN hpen = CreatePen(PS_SOLID, thickness, CrossHairColor);
+    HPEN holdpen = (HPEN)SelectObject(hdc, hpen);
+    int halflength = length / 2;  // Corrected calculation
+    MoveToEx(hdc, centerX - halflength, centerY, NULL);
+    LineTo(hdc, centerX + halflength, centerY); // Horizontal line
+    MoveToEx(hdc, centerX, centerY - halflength, NULL);
+    LineTo(hdc, centerX, centerY + halflength); // Vertical line
+    SelectObject(hdc, holdpen);
+    DeleteObject(hpen);
+}
 
-	int halflength = length / -1;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        int cx = ScreenX / 2;
+        int cy = ScreenY / 2;
+        DrawCrosshair(hdc, cx, cy, 9, 3);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    case WM_DESTROY:
+        Running = false;
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
 
-	MoveToEx(hdc, centerX - halflength, centerY, 0); LineTo(hdc, centerX + halflength, centerY); //gorizontalniy
-	MoveToEx(hdc, centerX, centerY - halflength, 0); LineTo(hdc, centerX, centerY + halflength); //vertiKALLniy
-
-	SelectObject(hdc, holdpen);
-	DeleteObject(hpen);
+void KeyCheckThread()
+{
+    while (Running)
+    {
+        if (GetAsyncKeyState(VK_END) & 0x8000)
+        {
+            Beep(275, 100);
+            Running = false;
+            break;
+        }
+        Sleep(10);
+    }
 }
 
 int main()
 {
-	SetConsoleTitleA("nu sho rodnoy pokazivay");
+    SetConsoleTitleA("Crosshair Overlay");
 
-	WNDCLASS wc = {};
-	wc.lpfnWndProc = DefWindowProc;
-	wc.hInstance = GetModuleHandle(0);
-	wc.lpszClassName = L"CrosshairWindow";
+    // Register window class
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = L"CrosshairWindow";
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    RegisterClassEx(&wc);
 
-	RegisterClass(&wc);
+    // Create the window
+    HWND Window = CreateWindowEx(
+        WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW,
+        wc.lpszClassName,
+        L"CrosshairWindow",
+        WS_POPUP,
+        0, 0, ScreenX, ScreenY,
+        NULL, NULL, wc.hInstance, NULL
+    );
 
-	HWND Window = CreateWindowExW
-	(
-		WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED | WS_EX_TOOLWINDOW,
-		wc.lpszClassName, L"CrosshairWindow", WS_POPUP, 0, 0, ScreenX, ScreenY, 0, 0, wc.hInstance, 0
-	);
+    if (!Window)
+    {
+        std::cerr << "Failed to create window. Error: " << GetLastError() << std::endl;
+        return 1;
+    }
 
+    // Set window properties
+    SetLayeredWindowAttributes(Window, RGB(0, 0, 0), 0, LWA_COLORKEY);
+    ShowWindow(Window, SW_SHOW);
+    UpdateWindow(Window);
 
-	ShowWindow(Window, SW_SHOW);
+    // Start key check thread
+    std::thread keyThread(KeyCheckThread);
 
-	SetLayeredWindowAttributes(Window, RGB(0, 0, 0), 0, LWA_COLORKEY);
+    // Message loop
+    MSG msg = {};
+    while (Running)
+    {
+        // Force redraw the window periodically
+        InvalidateRect(Window, NULL, TRUE);
+        UpdateWindow(Window);
 
-	HDC hdc = GetDC(Window);
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+            {
+                Running = false;
+                break;
+            }
+        }
 
-	int cx = ScreenX / 2;
-	int cy = ScreenY / 2;
+        Sleep(16); // ~60 FPS refresh rate
+    }
 
-	if (true)
-	{
-		DrawCrosshair(hdc, cx, cy, 7, 3);
-	}
+    // Clean up
+    keyThread.join();
+    DestroyWindow(Window);
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
 
-	MSG msg = {};
-	while(true) 
-	{
-		if (GetAsyncKeyState(VK_END))
-		{
-			Beep(275, 100);
-			break;
-		}
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessageA(&msg);
-		}
-	}
-
-	ReleaseDC(Window, hdc);
-	DestroyWindow(Window);
-
-	return 0;
+    return 0;
 }
